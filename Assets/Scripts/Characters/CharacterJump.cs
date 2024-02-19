37a1a95300a4b23a34a4020de946f6fc5e1d57a0
lapiedranumerodos <maria.orgniani@estudiante.imagecampus.edu.ar>
2024-02-19 18:31:11 -0300

    level 2 design changes, wall jump test

--- Assets/Scripts/Characters/CharacterJump.cs
+++ Assets/Scripts/Characters/CharacterJump.cs
@@ -12,8 +12,16 @@ public class CharacterJump : MonoBehaviour
 
     [SerializeField] private BoxCollider2D boxCollider;
     [SerializeField] private LayerMask ground;
+    [SerializeField] private LayerMask wall;
 
     public VoidDelegateType onJump;
+
+    private bool isSliding;
+    [SerializeField] private float wallSlidingSpeed;
+
+    [SerializeField] private float wallJumpDuration;
+    [SerializeField] private float wallJumpForce;
+    private bool wallJumping;
 
     private void OnEnable()
     {
@@ -41,18 +49,67 @@ public class CharacterJump : MonoBehaviour
         isJumping = rb.velocity.y > .1f;
         isFalling = rb.velocity.y < -.1f;
 
-        if (shouldJump && IsGrounded())
+        if (shouldJump)
+        {
+            JumpingAction();
+        }
+
+        if(IsOnWall() && !IsGrounded() && !shouldJump)
+        {
+            isSliding = true;
+        }
+
+        else
+        {
+            isSliding = false;
+        }
+
+        if(isSliding)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+
+        if(wallJumping)
+        {
+            rb.AddForce(Vector2.up * wallJumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void JumpingAction()
+    {
+        if(IsGrounded())
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            shouldJump = false;
-
-            if (onJump != null)
-                onJump();
         }
+
+        else if(isSliding)
+        {
+            wallJumping = true;
+            Invoke("StopWallJump", wallJumpDuration);
+
+        }
+
+        shouldJump = false;
+
+        if (onJump != null)
+            onJump();
+    }
+
+    private void StopWallJump()
+    {
+        wallJumping = false;
     }
 
     private bool IsGrounded()
     {
-        return Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, .1f, ground);
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, .1f, ground);
+        return raycastHit.collider != null;
     }
+
+    private bool IsOnWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, new Vector2(transform.localScale.x, 0), .1f, wall);
+        return raycastHit.collider != null;
+    }
+
 }
